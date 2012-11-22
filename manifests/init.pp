openstack-archive/fuel-library

Rewrite nova_config provider to use ini_file.
Tie nova_config settings to corresponding nova.conf sections.

deployment/puppet/nova/manifests/init.pp
@@ -165,26 +165,26 @@ class nova(
     } else {
       fail("Invalid db connection ${sql_connection}")
     }
-    nova_config { 'sql_connection': value => $sql_connection }
+    nova_config { 'DEFAULT/sql_connection': value => $sql_connection }
   } else {
     Nova_config <<| title == 'sql_connection' |>>
   }
 
-  nova_config { 'image_service': value => $image_service }
+  nova_config { 'DEFAULT/image_service': value => $image_service }
  
   if $image_service == 'nova.image.glance.GlanceImageService' {
     if $glance_api_servers {
-      nova_config { 'glance_api_servers': value => $glance_api_servers }
+      nova_config { 'DEFAULT/glance_api_servers': value => $glance_api_servers }
     } else {
       # TODO this only supports setting a single address for the api server
       Nova_config <<| title == glance_api_servers |>>
     }
   }
 
-  nova_config { 'auth_strategy': value => $auth_strategy }
+  nova_config { 'DEFAULT/auth_strategy': value => $auth_strategy }
 
   if $rabbit_host {
-    nova_config { 'rabbit_host': value => $rabbit_host }
+    nova_config { 'DEFAULT/rabbit_host': value => $rabbit_host }
   }
   else {
      Nova_config <<| title == 'rabbit_host' |>>
@@ -198,33 +198,33 @@ class nova(
   }
   # I may want to support exporting and collecting these
   nova_config {
-    'rabbit_password':     value => $rabbit_password;
-    'rabbit_port':         value => $rabbit_port;
-    'rabbit_userid':       value => $rabbit_userid;
-    'rabbit_virtual_host': value => $rabbit_virtual_host;
-    'rpc_backend': value => 'nova.rpc.impl_kombu';
+    'DEFAULT/rabbit_password':     value => $rabbit_password;
+    'DEFAULT/rabbit_port':         value => $rabbit_port;
+    'DEFAULT/rabbit_userid':       value => $rabbit_userid;
+    'DEFAULT/rabbit_virtual_host': value => $rabbit_virtual_host;
+    'DEFAULT/rpc_backend': value => 'nova.rpc.impl_kombu';
   }
 
   nova_config {
-    'verbose':           value => $verbose;
-    'logdir':            value => $logdir;
+    'DEFAULT/verbose':           value => $verbose;
+    'DEFAULT/logdir':            value => $logdir;
     # Following may need to be broken out to different nova services
-    'state_path':        value => $state_path;
-    'lock_path':         value => $lock_path;
-    'service_down_time': value => $service_down_time;
-    'root_wrap_config':  value => $root_wrap_config;
+    'DEFAULT/state_path':        value => $state_path;
+    'DEFAULT/lock_path':         value => $lock_path;
+    'DEFAULT/service_down_time': value => $service_down_time;
+    'DEFAULT/root_wrap_config':  value => $root_wrap_config;
   }
 
   nova_config {
-    'ec2_listen':           value => $api_bind_address;
-    'osapi_compute_listen': value => $api_bind_address;
-    'metadata_listen':      value => $api_bind_address;
-    'osapi_volume_listen':  value => $api_bind_address;
+    'DEFAULT/ec2_listen':           value => $api_bind_address;
+    'DEFAULT/osapi_compute_listen': value => $api_bind_address;
+    'DEFAULT/metadata_listen':      value => $api_bind_address;
+    'DEFAULT/osapi_volume_listen':  value => $api_bind_address;
   }
 
   if $monitoring_notifications {
     nova_config {
-      'notification_driver': value => 'nova.notifier.rabbit_notifier'
+      'DEFAULT/notification_driver': value => 'nova.notifier.rabbit_notifier'
     }
   }
 

Mirantis/puppetlabs-nova

manifests/init.pp
@@ -5,7 +5,7 @@
 #
 # [sql_connection] Connection url to use to connect to nova sql database.
 #  If specified as false, then it tries to collect the exported resource
-#   Nova_config <<| title == 'sql_connection' |>>. Optional. Defaults to false.
+#   Nova_config <<| title == 'DEFAULT/sql_connection' |>>. Optional. Defaults to false.
 # [image_service] Service used to search for and retrieve images. Optional.
 #   Defaults to 'nova.image.local.LocalImageService'
 # [glance_api_servers] List of addresses for api servers. Optional.
@@ -136,51 +136,51 @@ class nova(
     } else {
       fail("Invalid db connection ${sql_connection}")
     }
-    nova_config { 'sql_connection': value => $sql_connection }
+    nova_config { 'DEFAULT/sql_connection': value => $sql_connection }
   } else {
-    Nova_config <<| title == 'sql_connection' |>>
+    Nova_config <<| title == 'DEFAULT/sql_connection' |>>
   }
 
-  nova_config { 'image_service': value => $image_service }
-
+  nova_config { 'DEFAULT/image_service': value => $image_service }
+ 
   if $image_service == 'nova.image.glance.GlanceImageService' {
     if $glance_api_servers {
-      nova_config { 'glance_api_servers': value => $glance_api_servers }
+      nova_config { 'DEFAULT/glance_api_servers': value => $glance_api_servers }
     } else {
       # TODO this only supports setting a single address for the api server
-      Nova_config <<| title == glance_api_servers |>>
+      Nova_config <<| title == 'DEFAULT/glance_api_servers' |>>
     }
   }
 
-  nova_config { 'auth_strategy': value => $auth_strategy }
+  nova_config { 'DEFAULT/auth_strategy': value => $auth_strategy }
 
   if $rabbit_host {
-    nova_config { 'rabbit_host': value => $rabbit_host }
+    nova_config { 'DEFAULT/rabbit_host': value => $rabbit_host }
   } else {
-    Nova_config <<| title == 'rabbit_host' |>>
+    Nova_config <<| title == 'DEFAULT/rabbit_host' |>>
   }
   # I may want to support exporting and collecting these
   nova_config {
-    'rabbit_password':     value => $rabbit_password;
-    'rabbit_port':         value => $rabbit_port;
-    'rabbit_userid':       value => $rabbit_userid;
-    'rabbit_virtual_host': value => $rabbit_virtual_host;
+    'DEFAULT/rabbit_password':     value => $rabbit_password;
+    'DEFAULT/rabbit_port':         value => $rabbit_port;
+    'DEFAULT/rabbit_userid':       value => $rabbit_userid;
+    'DEFAULT/rabbit_virtual_host': value => $rabbit_virtual_host;
+    'DEFAULT/rpc_backend': value => 'nova.rpc.impl_kombu';
   }
 
   nova_config {
-    'verbose':           value => $verbose;
-    'logdir':            value => $logdir;
+    'DEFAULT/verbose':           value => $verbose;
+    'DEFAULT/logdir':            value => $logdir;
     # Following may need to be broken out to different nova services
-    'state_path':        value => $state_path;
-    'lock_path':         value => $lock_path;
-    'service_down_time': value => $service_down_time;
-    'rootwrap_config':  value => $rootwrap_config;
+    'DEFAULT/state_path':        value => $state_path;
+    'DEFAULT/lock_path':         value => $lock_path;
+    'DEFAULT/service_down_time': value => $service_down_time;
+    'DEFAULT/rootwrap_config':   value => $rootwrap_config;
   }
-
 
   if $monitoring_notifications {
     nova_config {
-      'notification_driver': value => 'nova.notifier.rabbit_notifier'
+      'DEFAULT/notification_driver': value => 'nova.notifier.rabbit_notifier'
     }
   }
 